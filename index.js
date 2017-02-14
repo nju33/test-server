b546a3da1eb1f01e5e395de763e755b06d5f917f
nju33 <nju33.ki@gmail.com>
2017-02-15 04:59:34 +0900

    Fix

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-var http = require('http');
 var express = require('express');
 var app = express();
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.get('/', function (req, res) {
   res.send('Hello World');
 });
 
-var server = http.createServer(app);
-
-server.listen(server_port, server_ip_address, function() {
-  console.log("Listening on " + server_ip_address + ", server_port " + server_port);
-});
+app.listen(server_port, server_ip_address);
+console.log('Server running on http://%s:%s', server_port, server_ip_address);
