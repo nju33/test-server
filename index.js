8a6b4e9b1ae0cb6996dc50cc5cb908f34b314a3f
nju33 <nju33.ki@gmail.com>
2017-02-15 04:20:53 +0900

    Fix

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var http = require('http');
 var express = require('express');
 var app = express();
 
@@ -6,8 +7,10 @@ var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 app.get('/', function (req, res) {
   res.send('Hello World');
-})
+});
 
-app.listen(server_port, server_ip_address, function() {
+var server = http.createServer(app);
+
+server.listen(server_port, server_ip_address, function() {
   console.log("Listening on " + server_ip_address + ", server_port " + server_port);
 });
